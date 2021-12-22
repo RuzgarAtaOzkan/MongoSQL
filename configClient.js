c77d38113cc81b5ea9d9fcef1767fd09becadd95
RuzgarAtaOzkan <ruzgarataozkan@gmail.com>
2021-12-22 05:18:53 +0300

    initial commit

---
+++ configClient.js
@@ -0,0 +1,185 @@
+function validateProps(props) {
+  if (!props) {
+    throw new Error('Too few arguments specified for processProps');
+  }
+
+  if (typeof props !== 'object') {
+    throw new Error('Invalid type specified for props in validateProps');
+  }
+
+  const types = {
+    INT: 'INT',
+    VARCHAR255: 'VARCHAR(255)',
+  };
+
+  const commands = {
+    CREATE: 'CREATE',
+    TABLE: 'TABLE',
+    INSERT: 'INSERT',
+    INTO: 'INTO',
+  };
+
+  const propertyValues = Object.values(props);
+  const typeValues = Object.values(types);
+
+  for (let i = 0; i < propertyValues.length; i++) {
+    let ctr = 0;
+
+    for (let j = 0; j < typeValues.length; j++) {
+      if (propertyValues[i].includes(typeValues[j])) {
+        ctr = ctr + 1;
+      }
+    }
+
+    if (!ctr) {
+      throw new Error(
+        'Invalid type specified on property: ' + propertyValues[i]
+      );
+    }
+  }
+}
+
+function configClient(client) {
+  if (!client) {
+    throw new Error('Too few arguments specified in configClient');
+  }
+
+  const types = {
+    INT: 'INT',
+    VARCHAR255: 'VARCHAR(255)',
+  };
+
+  const commands = {
+    CREATE: 'CREATE',
+    TABLE: 'TABLE',
+    INSERT: 'INSERT',
+    INTO: 'INTO',
+  };
+
+  const db = {
+    createDb: function (name) {
+      if (!name) {
+        throw new Error('Too few arguments specified in db.createDb');
+      }
+
+      if (typeof name !== 'string') {
+        throw new Error('Invalid typeof parameter specified in db.createDb');
+      }
+
+      const sql = `CREATE DATABASE ${name}`;
+
+      const result = client.query(sql, (err, result) => {
+        if (err) {
+          return console.log(err.message);
+        }
+      });
+
+      return result;
+    },
+
+    createTable: function (name, options) {
+      if (!name || !options) {
+        throw new Error('Too few arguments specified in db.createTable');
+      }
+
+      if (typeof name !== 'string') {
+        throw new Error('Invalid typeof parameter specified in db.createTable');
+      }
+
+      const { properties } = options;
+
+      if (!properties) {
+        throw new Error('Missing properties of options in db.createTable');
+      }
+
+      validateProps(properties);
+
+      const keys = [];
+      for (const key in options.properties) {
+        keys.push(`${key} ${options.properties[key]}`);
+      }
+
+      const sql = `CREATE TABLE ${name} (${keys.join(', ')})`;
+
+      const result = client.query(sql, (err, result) => {
+        if (err) {
+          return console.log(err.message);
+        }
+      });
+
+      return result;
+    },
+
+    editTable: function (name, options) {
+      if (!name || !options) {
+        throw new Error('Too few arguments specified in db.createTable');
+      }
+
+      if (typeof name !== 'string') {
+        throw new Error('Invalid typeof parameter specified in db.createTable');
+      }
+
+      const { column } = options;
+
+      let sql = `ALTER TABLE ${name}`;
+
+      if (column) {
+        if (!column.type || !column.properties) {
+          throw new Error('Column properties are missing in db.editTable');
+        }
+
+        const keys = [];
+        for (const key in column.properties) {
+          keys.push(`${key} ${column.properties[key]}`);
+        }
+
+        sql = sql + ` ${column.type} COLUMN ${keys.join(', ')}`;
+
+        const result = client.query(sql, (err, result) => {
+          if (err) {
+            return console.log(err.message);
+          }
+        });
+
+        return result;
+      }
+    },
+    insertOne: function (tableName, doc) {
+      if (!doc || !tableName) {
+        throw new Error('Too few arguments specified in db.insertOne');
+      }
+
+      if (typeof tableName !== 'string') {
+        throw new Error('Invalid type specified for tableName in db.insertOne');
+      }
+
+      if (typeof doc !== 'object') {
+        throw new Error('Invalid type specified for doc in db.insertOne');
+      }
+
+      const keys = [];
+      const values = [];
+
+      for (const key in doc) {
+        keys.push(key);
+        values.push(`'${doc[key]}'`);
+      }
+
+      const sql = `INSERT INTO ${tableName} (${keys.join(
+        ', '
+      )}) VALUES (${values.join(', ')})`;
+
+      const result = client.query(sql, (err, result) => {
+        if (err) {
+          return console.log(err.message);
+        }
+      });
+
+      return result;
+    },
+  };
+
+  return db;
+}
+
+module.exports = configClient;
---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// MODULES
+const mysql = require('mysql');
+
+// LIBRARY
+const configClient = require('./configClient');
+
+const client = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'app',
+});
+
+client.connect((err) => {
+  if (err) {
+    throw console.log(err);
+  }
+
+  console.log('connected');
+});
+
+const db = configClient(client);
+
+const createTableResult = db.createTable('users', {
+  properties: {
+    id: 'INT AUTO_INCREMENT PRIMARY KEY',
+    address: 'VARCHAR(255)',
+    name: 'VARCHAR(255)',
+  },
+});
+
+const editTableResult = db.editTable('users', {
+  column: {
+    type: 'ADD',
+    properties: {
+      id: 'INT AUTO_INCREMENT PRIMARY KEY',
+    },
+  },
+});
+
+const user = {
+  address: 'Sahiltepe',
+  name: 'Ruzgar',
+};
+
+const insertOneResult = db.insertOne('users', user);
